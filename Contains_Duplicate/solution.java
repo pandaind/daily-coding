c275424b9999b5ac817c4760233412dd2989e144
pandaind <chittaranjan@hotmail.com>
2025-06-15 22:44:31 +0530

    practice: 15 jun 251

--- Contains_Duplicate/solution.java
+++ Contains_Duplicate/solution.java
@@ -1,5 +1,13 @@
 class Solution {
+
     public boolean containsDuplicate(int[] nums) {
+        int arrLength = nums.length;
+        int dLength = Arrays.stream(nums).boxed().distinct().toList().size();
+        return arrLength!=dLength;
+    }
+
+
+    public boolean containsDuplicate1(int[] nums) {
         Set<Integer> uniques = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (!uniques.add(nums[i])) {
